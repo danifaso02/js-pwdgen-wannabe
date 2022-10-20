8267e11d87e7c419dcaf45a05a4dc0a687124cf7
danifaso02 <danifaso02@gmail.com>
2022-10-20 09:37:15 +0200

    Aggiunta commenti

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 "use: strict";
-
+// DOMANDE
 const firstName = prompt("Inserisci il tuo nome!");
 const lastName = prompt("Ora inserisci il tuo cognome!");
 const favouriteColour = prompt("Ci siamo quasi! Ora il tuo colore preferito!");
